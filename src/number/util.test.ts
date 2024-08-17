12cbf81e9c4d2ec34d30f0a75f74333621ed3605
kytnacode <kytnacode@gmail.com>
2024-08-16 19:18:48 -0700

    Function for checking valid string numbers

---
+++ src/number/util.test.ts
@@ -0,0 +1,21 @@
+// Testing functions:
+import { describe, expect, test } from "vitest";
+
+import { isNumber } from "./util";
+
+describe("util", () => {
+  describe("isNumber", () => {
+    test.each([
+      ["124", true],
+      ["14.742", true],
+      ["154.3521.64", false],
+      ["hello world", false],
+      ['foo', false],
+      ['0000', true],
+    ])("isNumber('%s') should return %b", (text,  expected) => {
+      const actual = isNumber(text);
+
+      expect(actual).toBe(expected);
+    })
+  })
+})
