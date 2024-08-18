618b14b63672b316a150fdde8e32bcecf5359650
Alejandro Paz Gómez (a.k.a. Kytnacode) <kytnacode@gmail.com>
2024-08-18 08:17:58 -0700

    Feat/add base layout (#7)
    * Add react-icons, tailwind and tailwind-merge
    
    * Add tailwindcss config
    
    * Add tailwind directives and remove boilerplate
    
    * Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { twMerge } from 'tailwind-merge';
+import './App.css';
+import { useEffect, useState } from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { IoMdSunny, IoIosMoon } from 'react-icons/io';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkMode, setDarkMode] = useState(false);
+  const [page, setPage] = useState('');
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setPage(window.location.hash);
+    };
+
+    window.addEventListener('popstate', onLocationChange);
+
+    return () => {
+      window.removeEventListener('popstate', onLocationChange);
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main
+      className={twMerge(
+        darkMode && 'dark',
+        'w-screen h-screen bg-neutral-100 dark:bg-neutral-900 dark:text-neutral-100 flex flex-col',
+      )}>
+      <nav className='relative flex p-2 bg-none border-neutral-300 dark:border-gray-800 border-solid border-b-2 w-full justify-between items-center'>
+        <h1 className='font-bold text-2xl'>
+          <a href='/'>Jerarquía de Operaciones</a>
+        </h1>
+
+        <button
+          onClick={() => setDarkMode(!darkMode)}
+          className='text-xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+          {darkMode ? <IoIosMoon /> : <IoMdSunny />}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+        <button className='rounded-full bg-neutral-900 dark:bg-neutral-100 text-neutral-100 dark:text-neutral-900 px-3 py-1 hover:bg-neutral-700 dark:hover:bg-neutral-300'>
+          <a className='text-xl font-bold' href='/#about'>
+            Más Información
+          </a>
+        </button>
+      </nav>
+      <section className='w-full flex-grow'>
+        {page === '#about' && (
+          <div className='w-full h-full grid place-items-center'>
+            <div className='leading-8 w-1/3 flex flex-col gap-3'>
+              <h2 className='text-3xl font-bold'>Sobre mi</h2>
+              <p>
+                Esta página fue hecha por{' '}
+                <span className='font-bold'>Alejandro Paz Gómez</span> en 2024.
+              </p>
+              <a
+                href='https://github.com/KytnaCode'
+                className='text-2xl self-center'>
+                <FaGithub />
+              </a>
+            </div>
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
